62bda2dca212c293db2fab7b6bad9473468be3f4
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-08-31 17:21:15 +0300

    (feature): added database

---
+++ database_service.py
@@ -0,0 +1,68 @@
+from json_service import JSONService
+
+class CategoryAlreadyExistsException:
+    pass
+
+class DatabaseService():
+
+    __CATEGORIES_KEY = "categories"
+    __BUDGET_KEY = "budget"
+    __TRANSACTIONS_KEY = "transactions"
+
+    def get_user_data_if_exists(user_id):
+        user_id = str(user_id)
+        database = JSONService.get_json()
+
+        if user_id in database:
+            return database[user_id]
+        else:
+            return None
+
+    def save_user_data(user_id, user_data):
+        database = JSONService.get_json()
+        database[str(user_id)] = user_data
+        JSONService.save_json(database)
+
+    def register_user(user_id):
+        
+        user_data = DatabaseService.get_user_data_if_exists(user_id)
+        if not (user_data is None):
+            raise RuntimeError(f"The user {user_id} is already registered")
+        userdata = {
+            DatabaseService.__CATEGORIES_KEY : dict()
+        }
+        DatabaseService.save_user_data(user_id, userdata)
+
+    def get_user(user_id:int):
+        user_data = DatabaseService.get_user_data_if_exists(user_id)
+
+        if user_data is None:
+            DatabaseService.register_user(user_id)
+            user_data = user_data = DatabaseService.get_user_data_if_exists(user_id)
+        
+        return user_data
+
+    def add_category(user_id:int, category:str, budged = 0):
+        user_data = DatabaseService.get_user(user_id)
+        
+        categories = user_data[DatabaseService.__CATEGORIES_KEY]
+
+        if category in categories:
+            raise CategoryAlreadyExistsException(f"Category {category} of user {user_id} already exists!")
+        
+        categories[category] = {
+            DatabaseService.__BUDGET_KEY: budged,
+            DatabaseService.__TRANSACTIONS_KEY: dict()
+        }
+
+        DatabaseService.save_user_data(user_id, user_data)
+        
+        
+        
+        
+
+        
+    
+        
+        
+    
