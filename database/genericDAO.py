fac6f5ac6593a332213bd28afe8d6a7eecc13a00
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-09-28 18:42:56 +0300

    (sub-feature): added datetime<->str convertion to GenericDAO

--- database/genericDAO.py
+++ database/genericDAO.py
@@ -1,5 +1,18 @@
 import sqlite3
+import datetime
 
 class GenericDAO:
     def __init__(self, connection: sqlite3.Connection):
         self.__connection = connection
+
+    def datetime_to_str(dt: datetime.datetime) -> str:
+        if dt is None:
+            return None
+        else:
+            return dt.isoformat()
+    
+    def str_to_datetime(s: str) -> datetime.datetime:
+        if s is None:
+            return None
+        else:
+            return datetime.datetime.fromisoformat(s)
