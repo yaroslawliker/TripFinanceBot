f9b9269afbe505949a101b474ef27022405292b2
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-09-29 17:29:06 +0300

    (sub-feature): added date convertion methods to GenericDAO

--- database/genericDAO.py
+++ database/genericDAO.py
@@ -5,6 +5,18 @@ class GenericDAO:
     def __init__(self, connection: sqlite3.Connection):
         self._connection = connection
 
+    def date_to_str(dt: datetime.date) -> str:
+        if dt is None:
+            return None
+        else:
+            return dt.isoformat()
+    
+    def str_to_date(s: str) -> datetime.date:
+        if s is None:
+            return None
+        else:
+            return datetime.date.fromisoformat(s)
+
     def datetime_to_str(dt: datetime.datetime) -> str:
         if dt is None:
             return None
