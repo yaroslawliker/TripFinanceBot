f73e247b6784c673a812a7b0152d098778a1a4c4
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-09-29 17:44:47 +0300

    (fix): fixed categories.sql missing user_id column before making it foreign key

--- database/tables/categories.sql
+++ database/tables/categories.sql
@@ -4,5 +4,6 @@ CREATE TABLE categories (
     budget REAL DEFAULT 0,
     start_date TEXT,
     end_date TEXT,
-    FOREIGN KEY(user_id) REFERENCES users(chat_id) NOT NULL
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(chat_id)
 )
