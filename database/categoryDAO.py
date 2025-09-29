e203cc91eddf49d666b6298e0073ef65f4a0a79c
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-09-29 17:37:55 +0300

    (fix): fixed _category_from_row

--- database/categoryDAO.py
+++ database/categoryDAO.py
@@ -5,13 +5,13 @@ from database.genericDAO import GenericDAO
 
 class CategoryDAO(GenericDAO):
 
-    def __category_from_row(row):
+    def _category_from_row(row):
         
         id = row[0]
         name = row[1]
         budget = row[2]
-        start_date = GenericDAO.date_to_str(row[3])
-        end_date = GenericDAO.date_to_str(row[4])
+        start_date = GenericDAO.str_to_date(row[3])
+        end_date = GenericDAO.str_to_date(row[4])
         user_id = row[5]
 
         return Category(id, name, budget, start_date, end_date, user_id)
@@ -34,7 +34,7 @@ class CategoryDAO(GenericDAO):
         if row == None:
             return 
         else:
-            return CategoryDAO.__category_from_row(row)
+            return CategoryDAO._category_from_row(row)
         
     
     def get_all_categories_of_user(self, chat_id:int) -> list:
@@ -43,7 +43,7 @@ class CategoryDAO(GenericDAO):
         result = self._connection.execute("SELECT * FROM categories WHERE chat_id = ?", (chat_id,)).fetchall()
 
         for row in result:
-            categories.append(CategoryDAO.__category_from_row(row))
+            categories.append(CategoryDAO._category_from_row(row))
 
         return categories
 
