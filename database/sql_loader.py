24cc5c79e7346ea1ad195f44ad4c1fde1a2ad85a
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-09-29 17:08:31 +0300

    (fix): fixed sql_loader sql folder pqth

--- database/sql_loader.py
+++ database/sql_loader.py
@@ -1,7 +1,7 @@
-SQL_FILDER_PATH = "/database/tables/"
+SQL_FOLDER_PATH = "database/tables/"
 
 def load_sql_from_file(filename: str):
-    filepath = SQL_FILDER_PATH + filename
+    filepath = SQL_FOLDER_PATH + filename
     with open(filepath, "rt") as file:
         result = file.read()
     return result
