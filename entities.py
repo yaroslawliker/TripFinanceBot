add1de7a72caf2aec806ed5b8ec21d0b354aba11
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-28 15:08:48 +0200

    (feat): add is_dated in Category and __str__, __repr__ to Expense

--- entities.py
+++ entities.py
@@ -1,7 +1,6 @@
 import datetime
 
 
-
 class User:
     def __init__(self, chat_id:int):
         self.chat_id = chat_id
@@ -17,7 +16,10 @@ class Category:
         self.start_date = start_date
         self.end_date = end_date
         self.chat_id = user_id
-
+    
+    def is_dated(self) -> bool:
+        """Checks if end_date and start_end are set."""
+        return self.start_date is not None
 
 class Expense:
     def __init__(self, id: int, money: float, datetime: datetime.datetime, purpose: str, category_id: int):
@@ -26,3 +28,12 @@ class Expense:
         self.datetime = datetime
         self.purpose = purpose
         self.category_id = category_id
+
+    def __repr__(self):
+        return (f"Expense(id={self.id}, money={self.money}, "
+                f"date_time={self.datetime!r}, purpose={self.purpose!r}, "
+                f"category_id={self.category_id})")
+
+    def __str__(self):
+        return (f"Expense #{self.id}: {self.money} грн, "
+                f"{self.datetime}, {self.purpose}, category={self.category_id}")
