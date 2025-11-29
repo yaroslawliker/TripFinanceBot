bef56276623dd845d8acd31371362c198efc26b8
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-30 01:19:02 +0200

    (feat): add per day statistics

--- handling/statistics/model.py
+++ handling/statistics/model.py
@@ -1,12 +1,13 @@
 """
 Module for retrieving and sorting week expense data and calculation statistics for it.
 """
-import datetime
-
 from dataclasses import dataclass
 
+import datetime
+from statistics import mean
+
 from database.database_service import DatabaseService
-from entities import Category
+from entities import Category, Expense
 
 
 ###
@@ -18,12 +19,12 @@ class NoExpensesException:
 
 @dataclass
 class WeekExpensesDTO:
-    week_start: datetime
-    week_end: datetime
+    start: datetime.date
+    end: datetime.date
     expenses: list
 
     def days(self) -> datetime.timedelta:
-        return self.week_end - self.week_start
+        return self.end - self.start
     
 @dataclass
 class WeekStatisicsDTO:
@@ -79,8 +80,8 @@ def init_week_edges(start_date:datetime.date, end_date: datetime.date):
         cur_mon = cur_mon + datetime.timedelta(days=7)
 
     # Ensuring edges
-    weeks[0].week_start = start_date
-    weeks[-1].week_end = end_date
+    weeks[0].start = start_date
+    weeks[-1].end = end_date
 
     return weeks
 
@@ -91,7 +92,7 @@ def split_into_weeks(expenses, weeks) -> None:
         """Updates week"""
 
         def is_in_week(date, week):
-            return date >= week.week_start and date <= week.week_end
+            return date >= week.start and date <= week.end
         
         if is_in_week(date, cur_week):
             return cur_week
@@ -110,17 +111,76 @@ def split_into_weeks(expenses, weeks) -> None:
 ### Calculating statistics
 ###
 
+### Per week statistics
+
 def calculate_week_total_expense(weeks):
+    """Calculates statistics about every week."""
     stats = []
 
     for week in weeks:
         week: WeekExpensesDTO
 
         # Getting days amount
-        days = (week.week_end - week.week_start).days + 1
+        days = (week.end - week.start).days + 1
         # Getting money sum
         total = sum(map(lambda e: e.money, week.expenses))
 
         stats.append(WeekStatisicsDTO(total, days))
     
     return stats
+
+
+### Per weekday statistics
+
+def get_expenses_of_weekday(week:WeekExpensesDTO, day_num):
+    """
+    Gets expenses of the given day.
+    day_num is the number of the weekday (0 for monday, 6 for sunday)
+    returns the expenses list or None is day is out of the week.
+    """
+
+    if week.start.weekday() != 0:
+        mon = week.start - datetime.timedelta(days=week.start.weekday())
+    else:
+        mon = week.start
+    day = mon + datetime.timedelta(days=day_num)
+
+    # In case the day is not in the week
+    if (day < week.start or day > week.end):
+        return None
+        
+    result = []
+    for expense in week.expenses:
+        expense: Expense
+        if expense.datetime.date() == day:
+            result.append(expense)
+    return result
+
+def calculate_weekdays_evarage(weeks):
+    """Calculates average on every weekday"""
+
+    weekdays_money = [[] for i in range(7)]
+    for i in range(7):
+
+        for week in weeks:
+            expenses = get_expenses_of_weekday(week, i)
+            # The weedays is not in the week (week is not full)
+            if expenses == None:
+                continue
+
+            if len(expenses) == 0:
+                weekdays_money[i].append(0)
+            else:
+                moneys = [expense.money for expense in expenses]
+                weekdays_money[i] += moneys
+
+    averages = [
+        mean(day) if len(day) != 0 else 0 
+        for day in weekdays_money
+    ]
+
+    print(weekdays_money)
+
+    return averages
+            
+    
--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,7 +1,9 @@
 import unittest
 import datetime
 
-from handling.statistics.model import init_week_edges, split_into_weeks, WeekExpensesDTO, calculate_week_total_expense
+from handling.statistics.model import init_week_edges, split_into_weeks, \
+    WeekExpensesDTO, calculate_week_total_expense, \
+    get_expenses_of_weekday, calculate_weekdays_evarage
 from entities import Expense
 
 class TestWeekSorting(unittest.TestCase):
@@ -21,8 +23,8 @@ class TestWeekSorting(unittest.TestCase):
         ]
 
         for week, (exp_start, exp_end) in zip(weeks, expected):
-            self.assertEqual(week.week_start, exp_start)
-            self.assertEqual(week.week_end, exp_end)
+            self.assertEqual(week.start, exp_start)
+            self.assertEqual(week.end, exp_end)
 
     def test_split_into_weeks(self):
         start = datetime.date(2025, 11, 1)
@@ -51,17 +53,17 @@ class TestWeekSorting(unittest.TestCase):
         
         # --- Creating weeks ---
         # First week
-        start_1 = datetime.date(2025, 11, 1)
-        end_1 = datetime.date(2025, 11, 8)
+        start_1 = datetime.date(2025, 11, 3)
+        end_1 = datetime.date(2025, 11, 9)
         
-        exp1 = Expense(1, 100.5, datetime.datetime(2025, 11, 2), "Food", 1)
-        exp2 = Expense(2, 50.0, datetime.datetime(2025, 11, 3), "Taxi", 1)
+        exp1 = Expense(1, 100.5, datetime.datetime(2025, 11, 4), "Food", 1)
+        exp2 = Expense(2, 50.0, datetime.datetime(2025, 11, 5), "Taxi", 1)
         
         week_1_dto = WeekExpensesDTO(start_1, end_1, [exp1, exp2])
 
         # Second week
-        start_2 = datetime.date(2025, 11, 8)
-        end_2 = datetime.date(2025, 11, 11)
+        start_2 = datetime.date(2025, 11, 10)
+        end_2 = datetime.date(2025, 11, 12)
         
         week_2_dto = WeekExpensesDTO(start_2, end_2, [])
 
@@ -79,6 +81,93 @@ class TestWeekSorting(unittest.TestCase):
         # Second week
         self.assertEqual(stats[1].days, 3)
         self.assertEqual(stats[1].total, 0.0)
+    
+    def test_get_expenses_of_weekday(self):
+
+        e1 = Expense(1, 130, datetime.datetime(2025, 11, 26), None, -1)
+        e2 = Expense(2, 50, datetime.datetime(2025, 11, 26), None, -1)
+        e3 = Expense(3, 25, datetime.datetime(2025, 11, 27), None, -1)
+
+        week = WeekExpensesDTO(
+            datetime.date(2025, 11, 24),
+            datetime.date(2025, 11, 30),
+            [e1, e2, e3]
+        )
+
+        wed = get_expenses_of_weekday(week, 2)
+        thu = get_expenses_of_weekday(week, 3)
+        fri = get_expenses_of_weekday(week, 4)
+
+        self.assertEqual([e1, e2], wed)
+        self.assertEqual([e3], thu)
+        self.assertEqual([], fri)
+    
+    def test_get_expenses_of_weekday_not_full_week(self):
+
+        e1 = Expense(1, 130, datetime.datetime(2025, 11, 26), None, -1)
+        e2 = Expense(2, 50, datetime.datetime(2025, 11, 26), None, -1)
+        e3 = Expense(3, 25, datetime.datetime(2025, 11, 27), None, -1)
+
+        week = WeekExpensesDTO(
+            datetime.date(2025, 11, 26),
+            datetime.date(2025, 11, 28),
+            [e1, e2, e3]
+        )
+
+        mon = get_expenses_of_weekday(week, 0)
+        tue = get_expenses_of_weekday(week, 1)
+        wed = get_expenses_of_weekday(week, 2)
+        thu = get_expenses_of_weekday(week, 3)
+        fri = get_expenses_of_weekday(week, 4)
+        sat = get_expenses_of_weekday(week, 5)
+        sun = get_expenses_of_weekday(week, 6)
+
+        self.assertEqual([e1, e2], wed)
+        self.assertEqual([e3], thu)
+        self.assertEqual([], fri)
+        self.assertEqual(None, mon)
+        self.assertEqual(None, tue)
+        self.assertEqual(None, sat)
+        self.assertEqual(None, sun)
+
+    def test_calculate_weekdays_average(self):
+
+        e1 = Expense(1, 130, datetime.datetime(2025, 11, 26), None, -1) # Wed
+        e2 = Expense(2, 50, datetime.datetime(2025, 11, 26), None, -1) # Wed
+        e3 = Expense(3, 25, datetime.datetime(2025, 11, 27), None, -1) # Thu
+
+        week1 = WeekExpensesDTO(
+            datetime.date(2025, 11, 26),
+            datetime.date(2025, 11, 28),
+            [e1, e2, e3]
+        )
+
+        e4 = Expense(1, 45, datetime.datetime(2025, 12, 1), None, -1) # Mon
+        e5 = Expense(2, 15.5, datetime.datetime(2025, 12, 3), None, -1) # Wed
+        e6 = Expense(3, 25.3, datetime.datetime(2025, 12, 4), None, -1) # Thu
+
+        week2 = WeekExpensesDTO(
+            datetime.date(2025, 12, 1),
+            datetime.date(2025, 12, 4),
+            [e4, e5, e6]
+        )
+        expected = [
+            e4.money,
+            0,
+            (e1.money + e2.money + e5.money) / 3,
+            (e3.money + e6.money) / 2,
+            0,
+            0,
+            0
+        ]
+
+        # Act
+        weeks = [week1, week2]
+        result = calculate_weekdays_evarage(weeks)
+
+        # Assert
+        self.assertEqual(expected, result)
+
 
 
 
